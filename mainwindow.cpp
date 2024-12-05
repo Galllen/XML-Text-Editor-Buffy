5570d10ad6ced73b9e751e4191dd7921c5154b53
Galllen <136637248+Galllen@users.noreply.github.com>
2024-12-05 22:51:17 +0300

    v0.13

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,14 +3,20 @@
 #include <QMessageBox>
 #include <QTextStream>
 #include <QFile>
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QTabWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setCentralWidget(ui->textEdit);
-    ui->statusbar->showMessage("Ok");
+
+    tabWidget = new QTabWidget(this);
+    setCentralWidget(tabWidget);
+
+    ui->statusbar->showMessage("Ok",5000);
     this->setWindowTitle("beefy's");
 }
 
@@ -19,42 +25,104 @@ MainWindow::~MainWindow()
 
     delete ui;
 }
+
+
 QString baseName;
+QString fileName;
+
 void MainWindow::on_actionOpen_triggered()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, "Open File", ".txt","All files (*.*);;Text files (*.txt);;Configuration files (*.ini *.xml *.json *.yaml *.yml *.toml *.conf *.cfg)");
+    fileName = QFileDialog::getOpenFileName(this, "Open File", ".txt","All files (*.*);;Text files (*.txt);;Configuration files (*.ini *.xml *.json *.yaml *.yml *.toml *.conf *.cfg)");
     if (fileName.isEmpty()) {
         return;
     }
-    baseName = QFileInfo(fileName).fileName();
+
     QFile file(fileName);
-    file.open(QIODevice::ReadOnly);
+    if (!file.open(QIODevice::ReadOnly)) {
+        QMessageBox::critical(this, "Error", "Could not open file for reading.");
+        return;
+    }
     QString log = file.readAll();
-    ui->textEdit->setText(log);
-    file.flush();
     file.close();
-    this->setWindowTitle(baseName +" beefy's");
+
+    QTextEdit *textEdit = new QTextEdit(this);
+    textEdit->setText(log);
+    int index = tabWidget->addTab(textEdit, QFileInfo(fileName).fileName());
+    tabWidget->setCurrentIndex(index);
+
+    fileMap[fileName] = textEdit;
+
+    baseName = QFileInfo(fileName).fileName();
+    this->setWindowTitle(QFileInfo(fileName).fileName() + " beefy's");
+    ui->statusbar->showMessage("File opened",5000);
 }
+
+
 
 void MainWindow::on_actionSave_triggered()
 {
-        QString fileName = QFileDialog::getSaveFileName(this, "Save File",baseName);
-        if (fileName.isEmpty()) {
-            return;
+
+    QString fileName = QFileDialog::getSaveFileName(this, "Save File",baseName);
+    if (fileName.isEmpty()) {
+        return;
+    }
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly)) {
+        QMessageBox::critical(this, "Error", "Could not open file for writing.");
+        return;
+    }
+
+    QTextEdit *currentTextEdit = qobject_cast<QTextEdit*>(tabWidget->currentWidget());
+    if (!currentTextEdit) {
+        QMessageBox::critical(this, "Error", "No active text editor found.");
+        return;
+    }
+
+    QTextStream out(&file);
+    out << currentTextEdit->toPlainText();
+    file.flush();
+    file.close();
+
+}
+
+void MainWindow::on_actionSave_2_triggered()
+{
+    // Получаем указатель на текущую активную вкладку
+    QTextEdit *currentTextEdit = qobject_cast<QTextEdit*>(tabWidget->currentWidget());
+    if (!currentTextEdit) {
+        QMessageBox::critical(this, "Error", "No active text editor found.");
+        return;
+    }
+
+    // Ищем имя файла, связанное с текущей вкладкой
+    QString fileName;
+    for (auto it = fileMap.begin(); it != fileMap.end(); ++it) {
+        if (it.value() == currentTextEdit) {
+            fileName = it.key();
+            break;
         }
-        QFile file(fileName);
-        if (!file.open(QIODevice::WriteOnly)) {
-            QMessageBox::critical(this, "Error", "Could not open file for writing.");
-            return;
-        }
-        QTextStream out(&file);
-        out << ui->textEdit->toPlainText();
-        file.flush();
-        file.close();
+    }
+
+    if (fileName.isEmpty()) {
+        QMessageBox::critical(this, "Error", "No file associated with the current tab.");
+        return;
+    }
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly)) {
+        QMessageBox::critical(this, "Error", "Could not open file for writing.");
+        return;
+    }
+
+    QTextStream out(&file);
+    out << currentTextEdit->toPlainText();
+    file.flush();
+    file.close();
+
+    ui->statusbar->showMessage("File saved", 5000);
 }
 
 void MainWindow::on_actionExit_triggered()
 {
     close();
 }
-
--- mainwindow.h
+++ mainwindow.h
@@ -1,13 +1,15 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-#include <QFileDialog>
 
 #include <QMainWindow>
+#include <QTabWidget>
+#include <QMap>
+#include <QTextEdit>
+#include <QFileInfo>
+#include <QFileDialog>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -22,8 +24,14 @@ private slots:
     void on_actionOpen_triggered();
     void on_actionSave_triggered();
     void on_actionExit_triggered();
+    void closeTab(int index);
+    void on_actionSave_2_triggered();
 
 private:
     Ui::MainWindow *ui;
+    QTabWidget *tabWidget;
+    QFileDialog FileDialog;
+    QMap<QString, QTextEdit*> fileMap;
 };
+
 #endif // MAINWINDOW_H
